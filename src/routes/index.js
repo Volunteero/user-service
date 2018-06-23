4c5da57c0f5ed327840889063b1e304ffb056f53
Dmitry Orlov <dmitry-orlov@hotmail.com>
2018-06-24 01:08:22 +0200

    Revert "Revert "Auth middleware""

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,18 @@
 'use strict';
 
+const {handleError} = require('../middleware/ErrorHandling');
 const usersRouter = require('./users');
 
 module.exports = () => {
   return {
+    /**
+     * Adds the top level routing to the application
+     * @param {Object} app
+     * @return {Object}
+     */
     init(app) {
       app.use('/altar', usersRouter);
+      app.use(handleError);
       return app;
     },
   };
